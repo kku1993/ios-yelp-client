c723afc4977a9deb2dc126583259a47cb75dd4c5
Kevin Ku <kku1993dev@gmail.com>
2014-06-21 12:01:57 -0700

    added xcode project

---
+++ YelpClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YelpClient
+//
+//  Created by Kevin Ku on 6/21/14.
+//  Copyright (c) 2014 Kevin Ku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YelpClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YelpClient
+//
+//  Created by Kevin Ku on 6/21/14.
+//  Copyright (c) 2014 Kevin Ku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
